9e24e596b3524c3797d8548079bd8ae4bf5780e6
assadalqaisi <assadalqaisi77@gmail.com>
2016-02-01 22:59:38 +0200

    Create credit.js

---
+++ master/credit.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#qaisi1web-prot')['html']('<a href="http://qaisi1web.blogspot.com" rel="dofollow" target="_blank">القيسي ويب | الأفضل لكم</a>');
+        setInterval(function () {
+                if (!$('#qaisi1web-prot:visible')['length']) {
+                  window['location']['href'] = 'http://qaisi1web.blogspot.com';
+                };
+            }, 3000);
+    });
+    //]]>
