685ba11b91c6272bec168cee8863d381ccd8e3e3
assadalqaisi <assadalqaisi77@gmail.com>
2016-02-05 16:42:57 +0200

    Update and rename templates-fekrat-meen.js to template-fekrat-meen.js

--- master/template-fekrat-meen.js
+++ master/template-fekrat-meen.js
@@ -1,8 +1,4 @@
-var thumbnail_mode = &quot;no-float&quot; ;
-summary_noimg = 300; /* طول الملخص للمواضيع بدون صورة */
-summary_img = 460; /* طول الملخص للمواضيع ذات صورة*/
-img_thumb_height = 210; /* عرض الصورة في الملخص*/
-img_thumb_width = 360; /*طول الصورة في الملخص */
+
 //<![CDATA[
 $(document)['ready'](function () {
         $('#qaisi1web-prot')['html']('<a href="http://qaisi1web.blogspot.com" rel="dofollow" target="_blank">القيسي ويب | الأفضل لكم</a>');
