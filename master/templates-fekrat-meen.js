d4c5d6cecec59c3de8966968c7ab9eb66764a50e
assadalqaisi <assadalqaisi77@gmail.com>
2016-02-05 16:38:20 +0200

    Create templates-fekrat-meen.js

---
+++ master/templates-fekrat-meen.js
@@ -0,0 +1,15 @@
+var thumbnail_mode = &quot;no-float&quot; ;
+summary_noimg = 300; /* طول الملخص للمواضيع بدون صورة */
+summary_img = 460; /* طول الملخص للمواضيع ذات صورة*/
+img_thumb_height = 210; /* عرض الصورة في الملخص*/
+img_thumb_width = 360; /*طول الصورة في الملخص */
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#qaisi1web-prot')['html']('<a href="http://qaisi1web.blogspot.com" rel="dofollow" target="_blank">القيسي ويب | الأفضل لكم</a>');
+        setInterval(function () {
+                if (!$('#qaisi1web-prot:visible')['length']) {
+                  window['location']['href'] = 'http://qaisi1web.blogspot.com';
+                };
+            }, 3000);
+    });
+//]]>
