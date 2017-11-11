e38d085992dd1eb30224c5d134897758106a7253
assadalqaisi <assadalqaisi77@gmail.com>
2017-11-11 06:10:05 +0200

    Update templates-old-post-qaisi1web.js

--- master/templates-old-post-qaisi1web.js
+++ master/templates-old-post-qaisi1web.js
@@ -1,14 +1,3 @@
-$(document).ready(function(){
-    $(&#39;[data-toggle=&quot;tooltip&quot;]&#39;).tooltip();
-});
-$(function () {
-  $(&#39;[data-toggle=&quot;popover&quot;]&#39;).popover();
-})
-$(&#39;#myTabs a&#39;).click(function (e) {
-  e.preventDefault()
-  $(this).tab(&#39;show&#39;)
-})
-
 var loriga = document.getElementById(&#39;old-post-info&#39;);
 var othmane = document.getElementById(&#39;new-post-info&#39;);
 othmane.innerHTML = loriga.innerHTML;
