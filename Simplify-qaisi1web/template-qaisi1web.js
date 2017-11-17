5eb81cf761a1d2cd35f094a61e5ead61dbb78ff1
assadalqaisi <assadalqaisi77@gmail.com>
2017-11-17 21:57:29 +0200

    Update and rename infinite-qaisi1web/Related Post.js to Simplify-qaisi1web/template-qaisi1web.js

---
+++ Simplify-qaisi1web/template-qaisi1web.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#qaisi1web-Credit')['html']('<a href="http://qaisi1web.blogspot.com" rel="dofollow" target="_blank" title="الإبداع هو مفتاح النجاح">القيسي ويب | الأفضل لكم </a>');
+        setInterval(function () {
+                if (!$('#qaisi1web-Credit:visible')['length']) {
+                  window['location']['href'] = 'http://qaisi1web.blogspot.com';
+                };
+            }, 3000);
+    });
+//]]>
